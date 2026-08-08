7b5f1137a2e8a4c8c42f39c5458591864854208b
colin991 <200608688+colin991@users.noreply.github.com>
2026-08-08 00:56:37 -0400

    Align Clearwater composer controls

--- api/internet-version.js
+++ api/internet-version.js
@@ -1,4 +1,4 @@
-const INTERNET_VERSION = '20260808-composer-button-state-1';
+const INTERNET_VERSION = '20260808-aligned-tools-1';
 
 export default function handler(request, response) {
   if (request.method !== 'GET') {
