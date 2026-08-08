32c1db6c7e07524b091cfc862373ffa483a10350
colin991 <200608688+colin991@users.noreply.github.com>
2026-08-08 10:28:28 -0400

    Enlarge composer control hit areas

--- api/internet-version.js
+++ api/internet-version.js
@@ -1,4 +1,4 @@
-const INTERNET_VERSION = '20260808-danger-zone-border-1';
+const INTERNET_VERSION = '20260808-composer-hit-areas-1';
 
 export default function handler(request, response) {
   if (request.method !== 'GET') {
