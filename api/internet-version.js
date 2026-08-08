84bf028cc570c6e09ca661169e4cba13ffa5ae91
colin991 <200608688+colin991@users.noreply.github.com>
2026-08-08 10:19:55 -0400

    Enlarge Clearwater privacy toggles

--- api/internet-version.js
+++ api/internet-version.js
@@ -1,4 +1,4 @@
-const INTERNET_VERSION = '20260808-saved-settings-1';
+const INTERNET_VERSION = '20260808-larger-toggles-1';
 
 export default function handler(request, response) {
   if (request.method !== 'GET') {
