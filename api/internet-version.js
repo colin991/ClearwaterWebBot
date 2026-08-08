f90e8cc17a94daa674555d35aa360cbf72253ebf
colin991 <200608688+colin991@users.noreply.github.com>
2026-08-08 10:57:07 -0400

    Restore danger zone top border

--- api/internet-version.js
+++ api/internet-version.js
@@ -1,4 +1,4 @@
-const INTERNET_VERSION = '20260808-message-bubbles-1';
+const INTERNET_VERSION = '20260808-danger-zone-border-1';
 
 export default function handler(request, response) {
   if (request.method !== 'GET') {
