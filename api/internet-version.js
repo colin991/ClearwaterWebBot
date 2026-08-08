409486b7ebd65ba29302b381f81004dcd5773c80
colin991 <200608688+colin991@users.noreply.github.com>
2026-08-08 12:57:12 -0400

    Keep account switcher in view

--- api/internet-version.js
+++ api/internet-version.js
@@ -1,4 +1,4 @@
-const INTERNET_VERSION = '20260808-official-account-1';
+const INTERNET_VERSION = '20260808-account-switcher-position-1';
 
 export default function handler(request, response) {
   if (request.method !== 'GET') {
