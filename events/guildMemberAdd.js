194270f174ec307f92e8a169a40286c42df71f9b
colin991 <200608688+colin991@users.noreply.github.com>
2026-08-10 21:46:58 -0400

    Unban Internet users when they rejoin Discord

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,29 @@
+import { Events } from 'discord.js';
+import { CLEARWATER_GUILD_ID, getHighestStaffRank } from '../utils/staffRanks.js';
+import { readInternetStore, saveInternetStore, setInternetBan, upsertInternetUser } from '../utils/internetStore.js';
+import { logger } from '../utils/logger.js';
+
+export default {
+  name: Events.GuildMemberAdd,
+  async execute(member) {
+    if (member.guild.id !== CLEARWATER_GUILD_ID) return;
+
+    const store = await readInternetStore();
+    const existingUser = store.users[member.id];
+    const staffRank = getHighestStaffRank(member);
+    const user = upsertInternetUser(store, {
+      id: member.id,
+      username: member.user.username,
+      displayName: member.user.globalName || member.user.username,
+      avatarUrl: member.user.displayAvatarURL({ extension: 'png', size: 64 }),
+      staffRank: staffRank?.name || null,
+    });
+
+    if (existingUser?.banned === true) {
+      setInternetBan(user, { enabled: false });
+      logger.info(`Unbanned ${member.user.username} from Clearwater Internet after they joined the Clearwater Discord server.`);
+    }
+
+    await saveInternetStore(store);
+  },
+};
