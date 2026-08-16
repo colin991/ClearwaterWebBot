dbe07f6b2da6ea461245f747e806e566f69b0e05
Colin <cdever126@gmail.com>
2026-08-16 12:09:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -59,4 +59,4 @@ process.on('uncaughtException', (error) => {
   process.exit(1);
 });
 
-await client.login(config.token); //gjjddddssssssssddssdsd
+await client.login(config.token); //gjjddddssssssssddssds
