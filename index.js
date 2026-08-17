de979714cce9313b98e3d072d67c29b82514135f
Colin <cdever126@gmail.com>
2026-08-16 20:33:18 -0400

    NO I FIXED ALL BUGS

--- index.js
+++ index.js
@@ -59,4 +59,4 @@ process.on('uncaughtException', (error) => {
   process.exit(1);
 });
 
-await client.login(config.token); //gjjddddssssssssddssddsa
+await client.login(config.token); //gjjddddssssssssddssddsad
