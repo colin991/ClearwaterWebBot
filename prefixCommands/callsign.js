405f91b9f49263097db8940f3b5837b0fd8efcf6
Colin <cdever126@gmail.com>
2026-09-07 19:21:13 -0400

    Fix PCSO roster activity checks

--- prefixCommands/callsign.js
+++ prefixCommands/callsign.js
@@ -1,23 +1,21 @@
 import { requireAdministrator } from '../utils/prefixHelpers.js';
-import { getHighestPinellasRank } from '../utils/pinellasPromote.js';
-import { buildPinellasCallsignPrompt } from '../utils/pinellasRoster.js';
+import { sendPinellasCallsignPanel, PINELLAS_CALLSIGN_CHANNEL_ID } from '../utils/pinellasRoster.js';
 import { PINELLAS_GUILD_ID } from '../utils/pinellasServer.js';
 
 export default {
   name: 'callsign',
   aliases: ['cs'],
-  description: 'Assign or update a PCSO callsign and roster entry (Administrator only).',
+  description: 'Post the public PCSO callsign panel (Administrator only).',
   async execute(message) {
     requireAdministrator(message);
     if (String(message.guild?.id) !== PINELLAS_GUILD_ID) {
       throw new Error('This command can only be used in the Pinellas County Sheriff\'s Office server.');
     }
 
-    const target = message.mentions.members.first() || message.member;
-    if (!target || target.user?.bot) throw new Error('Choose a server member, not a bot.');
-    const rank = getHighestPinellasRank(target);
-    if (!rank) throw new Error('That member does not have a supported PCSO rank role.');
-
-    await message.reply(buildPinellasCallsignPrompt(message.author.id, target));
+    const channel = message.guild.channels.cache.get(PINELLAS_CALLSIGN_CHANNEL_ID)
+      || await message.guild.channels.fetch(PINELLAS_CALLSIGN_CHANNEL_ID).catch(() => null);
+    if (!channel?.isTextBased?.()) throw new Error(`Callsign channel \`${PINELLAS_CALLSIGN_CHANNEL_ID}\` is unavailable.`);
+    await sendPinellasCallsignPanel(channel);
+    await message.reply('The public callsign panel was posted.');
   },
 };
